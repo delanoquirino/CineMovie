3a95ad9e42e7eb24ad845a7b07d9e3f1b51543ff
delanoquirino <delanoquirino@gmail.com>
2023-04-17 18:27:37 -0300

    StyledProvider import

--- src/pages/api/Providers/styles.js
+++ src/pages/api/Providers/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const div = styled.div`
-   color: black; 
+export const StyledProvider = styled.div`
+  color: black; 
   min-height: 100vh; 
   user-select: none; 
   z-index: 10; 
-  transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out; 
+  transition: color 0.3s ease-in-out, 
+  background-color 0.3s ease-in-out; 
 
   &.dark {
     color: #f8f8f8; 
